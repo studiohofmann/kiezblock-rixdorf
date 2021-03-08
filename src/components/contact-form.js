ad72c867c4247d100a21271e7087b718a92a24e3
Peter Hofmann <contact@firstfirst.design>
2021-03-08 17:04:32 +0100

    Contact

--- src/components/contact-form.js
+++ src/components/contact-form.js
@@ -5,21 +5,25 @@ import ComponentLayout from "./component-layout"
 const ContactFormStyle = styled.div`
   display: flex;
 
+  #form {
+    width: 100%;
+  }
+
   #name {
     width: 100%;
-    background-color: #a8ce80;
+    background-color: #a1d0e5;
     border: none;
   }
 
   #email {
     width: 100%;
-    background-color: #a8ce80;
+    background-color: #a1d0e5;
     border: none;
   }
 
   #message {
     width: 100%;
-    background-color: #a8ce80;
+    background-color: #a1d0e5;
     border: none;
     resize: none;
   }
@@ -34,39 +38,38 @@ export default function ContactForm() {
   return (
     <ComponentLayout>
       <ContactFormStyle>
-        <div>
-          <form
-            name="Contact Form"
-            method="POST"
-            data-netlify="true"
-            action="/message-success"
-          >
-            <input type="hidden" name="form-name" value="Contact Form" />
+        <form
+          id="form"
+          name="Contact Form"
+          method="POST"
+          data-netlify="true"
+          action="/message-success"
+        >
+          <input type="hidden" name="form-name" value="Contact Form" />
 
-            <div>
-              <label htmlFor="name">Dein Name</label>
-              <input type="text" name="name" id="name" />
-            </div>
+          <div>
+            <label htmlFor="name">Dein Name</label>
+            <input type="text" name="name" id="name" />
+          </div>
 
-            <div>
-              <label htmlFor="email">Deine Email</label>
-              <input type="text" name="email" id="email" />
-            </div>
+          <div>
+            <label htmlFor="email">Deine Email</label>
+            <input type="text" name="email" id="email" />
+          </div>
 
-            <div>
-              <label htmlFor="message">Deine Nachricht</label>
-              <textarea name="message" id="message" rows="6" />
-            </div>
-            <div id="buttonContainer">
-              <button type="submit" value="Send Message" className="special">
-                abschicken
-              </button>
-              <button type="reset" value="Clear">
-                löschen
-              </button>
-            </div>
-          </form>
-        </div>
+          <div>
+            <label htmlFor="message">Deine Nachricht</label>
+            <textarea name="message" id="message" rows="6" />
+          </div>
+          <div id="buttonContainer">
+            <button type="submit" value="Send Message" className="special">
+              abschicken
+            </button>
+            <button type="reset" value="Clear">
+              löschen
+            </button>
+          </div>
+        </form>
       </ContactFormStyle>
     </ComponentLayout>
   )
